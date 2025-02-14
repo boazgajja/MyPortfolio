675c8893aea81cf016c30a671f7e7b643707987f
boazgajja <boazgajja@gmail.com>
2025-02-14 14:40:37 +0530

    push

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MyPortfolio/', // 👈 Add this line (repo name)
+  base: '/MyPortfolio/', 
 });
